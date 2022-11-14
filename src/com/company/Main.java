4efa2816ec440560a9c8f38f3143bd07b940221b
Anna <anna.ostaptsuk1@gmail.com>
2022-11-14 20:59:36 +0200

    testing

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("                   \u2764\uFE0F                     ");
+        System.out.println("                   ***                              ");
+        System.out.println("                  *****                             ");
+        System.out.println("                 *******                            ");
+        System.out.println("                *********                           ");
+        System.out.println("               ***********                          ");
+        System.out.println("              *************                         ");
+        System.out.println("             ***************                        ");
+        System.out.println("                   **                               ");
+        System.out.println("                   **                               ");
+        System.out.println("Hello World \u2764");
+        System.out.println("Hello World \u2765");
+        System.out.println("Hello World \u2763");
+        System.out.println("\u2764\uFE0F");
+    }
+}
